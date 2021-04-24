19cd3b40b62e7796b1e90572e3082833cf84df50
Kai Hendry <hendry@iki.fi>
2021-04-24 18:16:38 +0800

    Tweaks

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 USER=ec2-user
 
 if ! test "$1"
@@ -13,10 +13,18 @@ IP=$(aws --profile dev cloudformation describe-stacks --stack-name benchmark \
 echo Copying test scripts to $USER@$IP
 scp bank-test.jmx jmeter.sh $USER@${IP}:
 
+ssh $USER@${IP} curl localhost:80
+
+if test $? != 0
+then
+	echo Service is not ready to test
+	exit
+fi
+
 ssh $USER@${IP} /home/$USER/jmeter.sh $1
 
-scp -r $USER@$IP:/home/$USER/$1 $1
+scp -qr $USER@$IP:/home/$USER/$1 $1
 
 URL=s.natalian.org/$(date -u +%Y-%m-%d)/$1
-aws s3 --profile mine sync --acl public-read $1 s3://$URL
+aws s3 --profile mine sync --quiet --acl public-read $1 s3://$URL
 echo https://$URL/index.html
